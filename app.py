67cf3e451b29c07a06e3688c726332ddcdc5c079
UtsavAdhikari3 <utsavadhikari99@gmail.com>
2025-07-03 16:16:36 +0545

    convert length func added + completed

--- app.py
+++ app.py
@@ -1,22 +1,61 @@
-from flask import Flask
-from flask import render_template, request
-from flask import jsonify
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
-@app.route("/",methods = ["GET"])
+
+@app.route("/", methods=["GET"])
 def hello():
-    return render_template("hello.html")
+    return render_template("hello.html")      
 
-
-@app.route("/read-form",methods = ["POST"])
+@app.route("/read-form", methods=["POST"])
 def read_form():
-    pass
+    if request.is_json:
+        data = request.get_json(force=True)
+    else:
+        data = request.form.to_dict()
 
+    form_id = data.get("formId", "length")  
+
+    if form_id == "length":
+        result = convert_length(data)
+    
+    elif form_id == "weight":
+        result = convert_weight(data)
+    reply = {
+        "formId": form_id,
+        "input":  data,
+        "result":result
+    }
+    return jsonify(reply), 200
 
 
 def convert_length(data):
-    print(data.fromLength)
+    if data['fromLength'] == "m" and data["to"] == "km":
+        value = float(data["lengthValue"])/1000
+
+    elif data['fromLength'] == "m" and data["to"] == "mi":
+        value = float(data["lengthValue"])/1609.344
+
+    elif data['fromLength'] == "km" and data["to"] == "m":
+        value = float(data["lengthValue"])*1000
+
+    elif data['fromLength'] == "km" and data["to"] == "mi":
+        value = float(data["lengthValue"])*0.621371
+
+    elif data['fromLength'] == "mi" and data["to"] == "m":
+        value = float(data["lengthValue"])*1609.344
+
+    elif data['fromLength'] == "mi" and data["to"] == "km":
+        value = float(data["lengthValue"])/0.621371
+    
+    else:
+        return "Value out of bounds"
+    
+    converted_length = {
+        "final_result" : "{:.2f}".format(value),
+    }
+    return converted_length        
 
 
-
+def convert_weight(data):
+    pass
